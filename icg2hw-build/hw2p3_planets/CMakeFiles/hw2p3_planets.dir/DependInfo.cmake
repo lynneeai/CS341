61497e7869c349a1c95487ed76bdb76a44b0b26c
wangmj17 <wangmj17@sina.com>
2016-03-04 11:55:53 +0100

    init

---
+++ icg2hw-build/hw2p3_planets/CMakeFiles/hw2p3_planets.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/wangmengjie/Desktop/graphics/icg2hw/hw2p3_planets/main.cpp" "/Users/wangmengjie/Desktop/graphics/icg2hw-build/hw2p3_planets/CMakeFiles/hw2p3_planets.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "GLEW_STATIC"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/Users/wangmengjie/Desktop/graphics/icg2hw/external"
+  "/Users/wangmengjie/Desktop/graphics/icg2hw/common"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
